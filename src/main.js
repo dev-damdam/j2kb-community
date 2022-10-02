2d0b23369fc14858088548d8e3338604802d1d0c
dev-damdam <dev.sodam@gmail.com>
2022-10-02 23:20:21 +0900

    add firebase, bootstrap on dependencies

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
 Vue.config.productionTip = false;
 
 new Vue({
