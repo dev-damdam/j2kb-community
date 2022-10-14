a69fe2ddb01cbc71b399a9630ee4fcecf88b50b7
dev-damdam <dev.sodam@gmail.com>
2022-10-14 23:50:37 +0900

    add MyMenuSide.vue

---
+++ src/views/home/MyMenuSide.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="my-menu-side-wrapper">
+    <b-list-group class="none-border-radius">
+      <b-list-group-item
+        v-for="menu in menuList"
+        :key="menu.link"
+        :active="menu.name == currentMenu.name"
+        @click="selectMenu(menu)"
+      >
+        {{ menu.name }}
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+<script>
+export default {
+  name: "my-menu-side",
+  props: {
+    menuList: {
+      type: Array,
+      require: true,
+    },
+  },
+  data() {
+    return {
+      currentMenu: {},
+    };
+  },
+  methods: {
+    selectMenu(menu) {
+      this.currentMenu = menu;
+      this.$emit("menu", menu);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.my-menu-side-wrapper {
+  width: 100%;
+
+  .none-border-radius {
+    border-radius: 0;
+  }
+}
+</style>
